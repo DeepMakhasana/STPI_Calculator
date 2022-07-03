58a8d1f97fc589c01031411b51b8553b1e19b511
Deep Makhasana <deepmakhasana1013@gmail.com>
2022-07-03 12:23:44 +0530

    edit index.php file

--- index.php
+++ index.php
@@ -68,7 +68,6 @@
     <footer class="bg-secondary">
         <div class="text-center py-3" style="color: white;">© 2021 Deep Makhasana, Inc. All rights reserved.</div>
     </footer>
-
     <!-- Javascript link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
